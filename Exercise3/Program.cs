4112ca09ebac6fd3ce4078835ca11dcba7681b8d
puglisac <alan.c.puglisi@gmail.com>
2021-01-07 09:29:27 -0600

    exercise complete

---
+++ Exercise3/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> integers = new List<int>();
+
+            //add integers 1-10 to the list
+            for(int i = 1; i<=10; i++)
+            {
+                integers.Add(i);
+            }
+
+            //print out all the integers in the list
+            for(int i = 0; i<integers.Count; i++)
+            {
+                Console.WriteLine(integers[i]);
+            }
+
+            //remove every other integer starting from the end of the list
+            for (int i = integers.Count-1; i > 0; i -= 2)
+            {
+                integers.RemoveAt(i);
+            }
+
+            //print out all the integers in the list
+            for (int i = 0; i < integers.Count; i++)
+            {
+                Console.WriteLine(integers[i]);
+            }
+
+            List<int> newList = new List<int>();
+
+            //add integers 1-5 to newList
+            for (int i = 1; i <= 5; i++)
+            {
+                newList.Add(i);
+            }
+
+            //remove the first three integers from newList
+            for (int i = 0; i<=2; i++)
+            {
+                newList.RemoveAt(i);
+            }
+
+            //print out all the integers in newList
+            for (int i = 0; i < newList.Count; i++)
+            {
+                Console.WriteLine(newList[i]);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,27 @@
+# Exercise 2: Lists
+
+This is an exercise for the coursera course [More C# Programming and Unity](https://www.coursera.org/learn/more-programming-unity)
+
+### Getting Started
+
+Double-click the index file in the Help folder and click the ConsoleCards link in the pane on the left; this is the documentation for the classes I provided to you in the ConsoleCards dll.
+
+Open the Exercise2 solution in your IDE.
+
+### Problem 1 - Convert from an array to a list
+
+Change the code in the \tt{Main}Main method to use a list of cards instead of an array.
+
+Remember, the **List** class is in the **System.Collections.Generic** namespace, so you may need to add a using directive for that namespace.
+ 
+## Installation
+To install, follow these steps:
+
+Via Downloading from GitHub:
+
+Download this repository onto your machine by clicking the "Clone or Download" button or Fork the repo into your own Github account
+Download and extract the zip file to a directory of your choice.  
+
+Via command line:
+
+`$ git clone https://github.com/puglisac/coursera c-sharp-2-ex2.git`
